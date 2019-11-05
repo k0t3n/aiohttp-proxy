2bb2d1f423afcaadfad54511c6ebed3f2fc3e242
k0t3n <k0t3n.mail@gmail.com>
2019-11-06 00:22:00 +0300

    Add replacement links to the current domain and improve word processing

--- server.py
+++ server.py
@@ -6,8 +6,29 @@ import html2text
 from aiohttp import ClientSession, web
 
 LIFEHACKER_URL = 'https://lifehacker.ru'
+BASE_URL = 'http://0.0.0.0:8080'
 EMOJI = ['😃', '😂', '😎', '😡', '😍', '😐']
 WORD_LENGTH = 6
+
+
+def change_links(string, old_link, new_link):
+    """
+    Changes all matched links from string to new link
+    :param string
+    :param new_link
+    :param old_link
+    :return: string with new links
+    """
+    return re.sub(rf'\b{old_link}\b', new_link, string)
+
+
+def clear_string_from_links(string):
+    """
+    Clear string from urls
+    :param string: string
+    :return: string without urls
+    """
+    return re.sub(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)', '', string)
 
 
 def validate_words(words, word_length):
@@ -18,12 +39,12 @@ def validate_words(words, word_length):
     :return: list of validated words of required length
     """
     validated_words = []
-    for word in words:
-        word_regexp = re.findall(r'\b\w+\b', word)  # extract only the word
-        if any(word_regexp):
-            word = word_regexp[0]
+    for raw_word in words:
+        words_regexp = re.findall(r'\b\w+\b', clear_string_from_links(raw_word))  # extract only the word
+        for word in words_regexp:
             if len(word) == word_length:
-                validated_words.append(word_regexp[0])
+                # print(word)
+                validated_words.append(word)
     return validated_words
 
 
@@ -32,9 +53,14 @@ async def proxy_with_emoji(request):
 
     async with ClientSession() as session:
         async with session.get(target_url) as resp:
+            content_type = resp.content_type
+            # return other content without modifying
+            if content_type != 'text/html':
+                return web.Response(body=await resp.read(), content_type=content_type)
             html = await resp.text()
 
-        words = validate_words(html2text.html2text(html).split(' '), WORD_LENGTH)
+        words = validate_words(html2text.html2text(html).replace('\n', '').split(' '), WORD_LENGTH)
+        html = change_links(html, LIFEHACKER_URL, BASE_URL)
         changed_words = set()
         for index, (word, emoji) in enumerate(zip(words, cycle(EMOJI))):
             if word not in changed_words:  # the page may contain duplicate words
